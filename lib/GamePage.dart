3873ba6a8c89068dd29c30d129a2453c11a17568
marco <marco.fornari@itssmartacademy.it>
2026-02-08 20:28:08 +0100

    characterHeader working fine with avatar

--- lib/GamePage.dart
+++ lib/GamePage.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  int selectedCharacterIndex = 0;
+  final List<Map<String, String>> characters = [
+    {
+      'name': 'Guts',
+      'image':
+          'https://wallpapercave.com/wp/wp2740235.jpg',
+    },
+    {'name': 'Griffith',
+     'image': 'https://i.redd.it/26dhb33dwgwb1.jpg'},
+    {
+      'name': 'Casca',
+      'image':
+          'https://preview.redd.it/why-so-cute-and-pretty-casca-v0-si3nrbluai5c1.jpg?width=630&format=pjpg&auto=webp&s=f6941ed658f2239294506f7844d481fd199d2edc',
+    },
+    {
+      'name': 'Zodd',
+      'image':
+          'https://preview.redd.it/what-is-your-opinion-on-zodd-v0-l7w9ki1xsgye1.jpeg?auto=webp&s=63c8950720bd77add831535553d1cc30c5d8f51f',
+    },
+  ];
 
   //**metodi colonna di sinistra**
   //Game Chat
@@ -90,14 +116,67 @@ class GamePage extends StatelessWidget {
 
   /*METODI COLONNA DI DESTRA*/
   //Character Header
+  Widget characterHeader() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(characters.length, (index) {
+            final bool isSelected = index == selectedCharacterIndex;
 
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedCharacterIndex = index;
+                });
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: isSelected ? Colors.white : Colors.transparent,
+                          width: 2,
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        radius: 22,
+                        backgroundImage: NetworkImage(
+                          characters[index]['image']!,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      characters[index]['name']!,
+                      style: TextStyle(
+                        color: isSelected ? Colors.white : Colors.white70,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
+      ],
+    );
+  }
+
+  //*Build*//
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: [
-          // left column
+          // LEFT COLUMN
           Expanded(
+            flex: 2,
             child: Container(
               color: Colors.grey.shade200,
               child: Column(
@@ -106,38 +185,32 @@ class GamePage extends StatelessWidget {
             ),
           ),
 
-          // right column
+          // RIGHT COLUMN
           Expanded(
             child: Container(
               color: Colors.grey.shade300,
               child: Column(
                 children: [
                   Container(
-                    height: 60,
-                    color: Colors.purple,
+                    height: 90,
+                    color: Color.fromRGBO(227, 0, 57, 1),
                     alignment: Alignment.center,
-                    child: const Text(
-                      'CharacterHeader',
-                      style: TextStyle(color: Colors.white),
-                    ),
+                    child: characterHeader(),
                   ),
-
                   SizedBox(
                     height: 200,
                     child: Image.network(
-                      'https://i.redd.it/26dhb33dwgwb1.jpg',
+                      characters[selectedCharacterIndex]['image']!,
                       width: double.infinity,
                       fit: BoxFit.cover,
                     ),
                   ),
-
                   Container(
                     height: 50,
                     color: Colors.yellow,
                     alignment: Alignment.center,
                     child: const Text('CharacterTabs'),
                   ),
-
                   Expanded(
                     child: Container(
                       color: Colors.white,
