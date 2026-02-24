a19bcb7e516e42790edae999017a3c4ad45fb43e
marco <marco.fornari@itssmartacademy.it>
2026-02-24 10:10:56 +0100

    pages divided in folders

---
+++ lib/user_pages/Registerpage.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'loginPage.dart';
+import '../services/auth_service.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final AuthService _authService = AuthService();
+
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 300),
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset('assets/images/logo.png'),
+
+                const SizedBox(height: 16),
+
+                //proviamo a mettere le righe
+                // name
+                TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
+                    hintText: 'Enter your name',
+                    prefixIcon: Icon(Icons.person_outline),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                // surname
+                TextFormField(
+                  controller: surnameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Surname',
+                    hintText: 'Enter your surname',
+                    prefixIcon: Icon(Icons.person_outline),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your name';
+                    }
+                    return null;
+                  },
+                ),
+
+                //chiusura prima riga
+
+                //const SizedBox(height: 8),
+
+                // email
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    prefixIcon: Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your email';
+                    }
+                    bool emailValid = RegExp(
+                      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                    ).hasMatch(value);
+                    if (!emailValid) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+
+                //const SizedBox(height: 8),
+
+                // password
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: _obscurePassword,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your password';
+                    }
+                    if (value.length < 8) {
+                      return 'At least 8 characters';
+                    }
+                    return null;
+                  },
+                ),
+
+                //const SizedBox(height: 8),
+
+                // confirm password
+                TextFormField(
+                  obscureText: _obscureConfirmPassword,
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                    hintText: 'Repeat your password',
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureConfirmPassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureConfirmPassword = !_obscureConfirmPassword;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Confirm your password';
+                    }
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+
+                const SizedBox(height: 16),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(227, 0, 58, 1),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        bool success = await _authService.register(
+                          nameController.text,
+                          surnameController.text,
+                          emailController.text,
+                          _passwordController.text,
+                        );
+
+                        if (success) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                "Registrazione completata con successo",
+                              ),
+                              backgroundColor: Colors.green,
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Errore durante la registrazione"),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      }
+                    },
+
+                    child: const Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'REGISTER',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 8),
+
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const userLogin(),
+                      ),
+                    );
+                  },
+                  child: const Text('Already have an account? Login'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/user_pages/recover_Password.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_404dungeon/user_pages/Registerpage.dart';
+import 'package:frontend_404dungeon/user_pages/forgot_Password.dart';
+import 'package:frontend_404dungeon/user_pages/loginPage.dart';
+import '../GamePage.dart';
+import 'package:frontend_404dungeon/services/auth_service.dart';
+
+//Create Form Widget
+class Recoverpage extends StatefulWidget {
+  const Recoverpage({super.key}); //costruttore del widget
+  //serve per identificare il widget nel grafo ad albero
+  @override
+  RecoverpageState createState() {
+    return RecoverpageState();
+  }
+}
+
+//create class
+class RecoverpageState extends State<Recoverpage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  final AuthService _authService = AuthService();
+  bool _obscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 300),
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // Vertical Axis
+              children: [
+                Image.asset('assets/images/logo.png'),
+
+                const SizedBox(height: 16),
+
+                //password
+                TextFormField(
+                  obscureText: _obscurePassword,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    border: OutlineInputBorder(),
+                    //toggle hidden pwd
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                    ),
+                  ),
+
+                  //obscure || visibile pwd
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your password';
+                    }
+                    if (value.length < 8) {
+                      return 'At Least 8 characters';
+                    }
+                    return null;
+                  },
+                ),
+
+                //spazio tra email e pwd
+                const SizedBox(height: 16),
+
+                //confirm password
+                TextFormField(
+                  obscureText: _obscurePassword,
+                  decoration: InputDecoration(
+                    labelText: 'Confirm email',
+                    hintText: 'Enter your password',
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    border: OutlineInputBorder(),
+                    //toggle hidden pwd
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                    ),
+                  ),
+
+                  //obscure || visibile pwd
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your password';
+                    }
+                    if (value.length < 8) {
+                      return 'At Least 8 characters';
+                    }
+                    return null;
+                  },
+                ),
+
+                const SizedBox(height: 16), //spazio tra pwd e login button
+
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(227, 0, 58, 1),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final token = Uri.base.queryParameters['token'];
+
+                        if (token == null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Token mancante o non valido"),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                          return;
+                        }
+
+                        bool success = await _authService.resetPassword(
+                          emailController.text.trim(),
+                          passwordController.text.trim(),
+                          token,
+                        );
+
+                        if (success) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Password aggiornata con successo"),
+                              backgroundColor: Colors.green,
+                            ),
+                          );
+
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Token non valido o scaduto"),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      }
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'LOGIN',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 5),
+
+                //create an Account
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('or create an Account'),
+                ),
+
+                const SizedBox(height: 5),
+
+                //forgot pwd
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ForgotPassword(),
+                      ),
+                    );
+                  },
+                  child: const Text('Forgot Password'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
